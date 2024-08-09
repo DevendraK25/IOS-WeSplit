ded414d41c11c5d73660ae13fbb4296c02d52fda
Devendra Kulkarni <devendrakulkarni@devendras-air.home>
2024-08-09 16:33:54 -0400

    Created WeSplit IOS App

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Devendra Kulkarni on 2024-08-05.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
